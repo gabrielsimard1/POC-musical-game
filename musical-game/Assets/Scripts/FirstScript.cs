ee606fdae1e00320c8595b2e77c0fcf964b81a9b
GarbeClara <clara.g-o@hotmail.fr>
2023-11-24 19:53:05 -0500

    Test commit

---
+++ musical-game/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Coucou");
+    }
+
+    void Update()
+    {
+        
+    }
+}
