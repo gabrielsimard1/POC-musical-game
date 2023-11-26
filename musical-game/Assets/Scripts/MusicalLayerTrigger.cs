fb440a0755e32fdf000d9d2d4a1cd368b66fec1e
GarbeClara <clara.g-o@hotmail.fr>
2023-11-26 18:18:01 -0500

    Adding velocity check for adding and removing layers
    Problem happened when we went back and forth on trigger; we can exit the trigger from the same side we entered it. Checking player velocity fixes it

--- musical-game/Assets/Scripts/MusicalLayerTrigger.cs
+++ musical-game/Assets/Scripts/MusicalLayerTrigger.cs
@@ -17,19 +17,29 @@ public class MusicalLayerTrigger : MonoBehaviour
         audioPlayer = FindObjectOfType<AudioPlayer>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    // this logic works assuming the trigger is a vertical block for a left to right movement. 
+    // at some point if we want to put horizontal triggers we might need to add a [SerializeField] bool to specify so and then we can adjust the logic (checking the velocity.y instead of x)
+    private void OnTriggerExit2D(Collider2D collision)
     { 
+        //get player moving direction with its velocity so we need to access its rigidbody2d
         if (gameObject.tag.Contains(LAYER_TRIGGER_PREFIX) && collision.CompareTag("Player"))
         {
+            //get player moving direction with its velocity so we need to access its rigidbody2d
+            Rigidbody2D playersRigidBody = collision.GetComponent<Rigidbody2D>();
+            Vector2 playersVelocity = playersRigidBody.velocity;
             (int layerIndex, int layerVariant) extractedIndexes = ExtractIndexes(gameObject.tag);
             int playerZone = gameSession.GetCurrentPlayerZone();
             // comments are assuming the scene is linear from left to right 
-            if (playerZone == extractedIndexes.layerIndex - 1) // means we're left from the trigger so we should add a music layer
+            if (playerZone == extractedIndexes.layerIndex - 1 && playersVelocity.x > 0) 
+                // means we're left from the trigger so we should add a music layer
+                // checking if player is moving from left to right otherwise if he goes back and forth on the trigger it messes up the playerzone
             {
-                audioPlayer.AddLayer(extractedIndexes.layerVariant);
-                gameSession.IncrementCurrentPlayerZone();
+               audioPlayer.AddLayer(extractedIndexes.layerVariant);
+               gameSession.IncrementCurrentPlayerZone();
             }
-            else // if playerzone == triggerIndex ==> means we were in the music layer zone and crossed the trigger (from right to left) so we should remove the music layer
+            else if (playerZone == extractedIndexes.layerIndex && playersVelocity.x < 0)
+                // if playerzone == triggerIndex ==> means we were in the music layer zone and crossed the trigger (from right to left) so we should remove the music layer
+                // checking if player is moving from right to left 
             {
                 audioPlayer.RemoveLayer(extractedIndexes.layerVariant);
                 gameSession.DecrementCurrentPlayerZone();
