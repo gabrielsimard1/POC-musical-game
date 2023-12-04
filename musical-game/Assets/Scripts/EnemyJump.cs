0ff405dc01657dcd05ed109df641df9daa160907
gabrielsimard1 <53264705+gabrielsimard1@users.noreply.github.com>
2023-12-04 12:01:15 -0500

    Feature/9 enemies pathfinding jumping (#24)
    * Fix wall stick jump
    
    * First draft of enemy pathfinding and jumping
    
    -jumping on imminent collision
    -jumping at player if chasing
    
    * First draft of enemy pathfinding and jumping
    
    -jumping on imminent collision
    -jumping at player if chasing
    
    * Bug fixes, prefabs
    
    * fix inaccurate feet collider enemy
    
    * Fix variable name for castpoint (PR)

---
+++ musical-game/Assets/Scripts/EnemyJump.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyJump : MonoBehaviour
+{
+    [Header("Game Objects")]
+    [SerializeField] Transform jumpOriginCastPoint;
+
+    [Header("Jump Variables")]
+    [SerializeField] float jumpHeight = 15f;
+
+    HashSet<string> playerTags = new() { "Player", "PlayerFeet" };
+
+    EnemyMovement enemyMovement;
+    Transform playerTransform;
+    float distToPlayer;
+    Rigidbody2D myRigidbody;
+    BoxCollider2D feetCollider;
+    float aggroRange;
+
+
+    void Awake()
+    {
+        enemyMovement = GetComponent<EnemyMovement>();
+    }
+
+    void Start()
+    {
+        myRigidbody = enemyMovement.GetRigidbody();
+        feetCollider = enemyMovement.GetFeetCollider();
+        aggroRange = enemyMovement.GetAggroRange();
+    }
+
+
+    void Update()
+    {
+        distToPlayer = enemyMovement.GetDistToPlayer();
+        playerTransform = enemyMovement.GetPlayer();
+    }
+
+    bool IsGrounded()
+    {
+        return feetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    }
+
+    public void JumpToPlayer()
+    {
+        Vector2 endPos = playerTransform.position - jumpOriginCastPoint.position;
+        RaycastHit2D hit = Physics2D.Raycast(jumpOriginCastPoint.position, endPos, distToPlayer, ~LayerMask.GetMask("Enemy", "Ground"));
+
+        if (hit.collider != null && playerTags.Contains(hit.collider.gameObject.tag))
+        {
+            float requiredJumpHeight = CalculateRequiredJumpHeight(playerTransform.position);
+            float jumpForceNeeded = CalculateJumpForceNeeded(myRigidbody.gravityScale, requiredJumpHeight);
+
+            if (jumpForceNeeded >= 2 && jumpForceNeeded <= jumpHeight && IsGrounded())
+            {
+                Jump();
+            }
+        }
+    }
+
+    public void JumpOnImminentCollision(float direction)
+    {
+        // TODO: Figure out better way to calculate endPos than aggroRange / 2
+        Vector2 endPos = new Vector2(direction, 0) * aggroRange / 2;
+        RaycastHit2D hit = Physics2D.Raycast(jumpOriginCastPoint.position, endPos, aggroRange / 2);
+
+        if (hit.collider != null &&
+            hit.collider.gameObject.CompareTag("Ground") &&
+            !playerTags.Contains(hit.collider.gameObject.tag))
+        {
+            Vector2 obstacleAboveHeight = CheckObstacleAbove(hit.point, direction);
+            if (obstacleAboveHeight != null)
+            {
+                float requiredJumpHeight = CalculateRequiredJumpHeight(obstacleAboveHeight);
+                float jumpForceNeeded = CalculateJumpForceNeeded(myRigidbody.gravityScale, requiredJumpHeight);
+
+                if (jumpForceNeeded <= jumpHeight && IsGrounded())
+                {
+                    Jump(1f);
+                }
+            }
+        }
+    }
+
+    Vector2 CheckObstacleAbove(Vector2 hitPoint, float direction)
+    {
+        Vector2 castOffset = new(direction * 0.1f, 0);
+        RaycastHit2D hitAbove = Physics2D.Raycast(hitPoint + castOffset, Vector2.up, LayerMask.GetMask("Ground"));
+
+        while (hitAbove.collider != null && hitAbove.collider.CompareTag("Ground"))
+        {
+            // Move the raycast upward
+            hitPoint = hitAbove.point + Vector2.up * 0.1f + castOffset;
+            hitAbove = Physics2D.Raycast(hitPoint, Vector2.up, 0.1f);
+        }
+
+        return hitPoint;
+    }
+
+    float CalculateRequiredJumpHeight(Vector2 obstaclePosition)
+    {
+        return Mathf.Max(0f, obstaclePosition.y - feetCollider.transform.position.y);
+    }
+
+    float CalculateJumpForceNeeded(float gravityScale, float jumpHeight)
+    {
+        return Mathf.Sqrt(2f * Mathf.Abs(gravityScale) * jumpHeight);
+    }
+
+    void Jump(float extraPushX = 1f)
+    {
+        float jumpForce = Mathf.Sqrt(2f * myRigidbody.gravityScale * jumpHeight);
+        float unstuckForce = 2f; 
+        myRigidbody.AddForce(new Vector2(-extraPushX * unstuckForce, jumpForce), ForceMode2D.Impulse);
+    }
+
+}
---
+++ musical-game/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Game Objects")]
+    [SerializeField] CircleCollider2D faceCollider;
+    [SerializeField] BoxCollider2D feetCollider;
+
+    [Header("Aggro")]
+    [SerializeField] Transform playerTransform;
+    [SerializeField][Tooltip("Value for enemy chase (both sides).")] float aggroRange = 4f;
+    [SerializeField] [Tooltip("Value for enemy chase once aggroed, chase doesn't stop unless distance between " +
+        "player and enemy is higher, then tether will break. Should not be higher " +
+        "than Aggro Range (can cause bugs).")]
+    float tetherToPlayerLength = 4f;
+
+    [Header("Movement")]
+    [SerializeField] float patrolMoveSpeed = 3f;
+    [SerializeField] float aggroMoveSpeed = 5f;
+    [SerializeField] float returnToSpawnSpeed = 6f;
+
+    EnemyJump enemyJump;
+    Vector2 spawnPos;
+    Rigidbody2D myRigidbody;
+
+    bool isReturningToSpawn;
+    float distToSpawnPos;
+    float distToPlayer;
+    const float distanceTolerance = 0.2f;
+    float directionTowardsPlayer = 0f;
+
+    bool canFlip = true;
+    const float flipCooldown = 0.3f;
+    float flipTimer;
+
+
+    public Transform GetPlayer() {  return playerTransform; }
+    public Rigidbody2D GetRigidbody() {  return myRigidbody; }
+    public BoxCollider2D GetFeetCollider() {  return feetCollider; }
+    public float GetDistToPlayer() {  return distToPlayer; }
+    public float GetAggroRange() {  return aggroRange; }
+
+
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        enemyJump = GetComponent<EnemyJump>();
+    }
+
+    void Start()
+    {
+        spawnPos = transform.position;
+    }
+
+    void Update()
+    {
+        UpdateDistances();
+        CurrentAction();
+        if (!canFlip)
+        {
+            FlipCooldown();
+        }
+    }
+
+    void FlipCooldown()
+    {
+        flipTimer += Time.deltaTime;
+        if (flipTimer >= flipCooldown)
+        {
+            canFlip = true;
+            flipTimer = 0f;
+        }
+    }
+
+    void CurrentAction()
+    {
+        if (PlayerIsInAggroRange() && !isReturningToSpawn)
+            ChasePlayer();
+        else if (isReturningToSpawn)
+            MoveToSpawn();
+        else
+            Patrol();
+    }
+
+    bool PlayerIsInAggroRange()
+    {
+        return distToPlayer <= aggroRange;
+    }
+
+    void UpdateDistances()
+    {
+        distToPlayer = Vector2.Distance(faceCollider.transform.position, playerTransform.position);
+        distToSpawnPos = Vector2.Distance(faceCollider.transform.position, spawnPos);
+
+        if (distToSpawnPos > aggroRange && distToPlayer >= tetherToPlayerLength)
+        {
+            isReturningToSpawn = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (faceCollider.IsTouchingLayers(LayerMask.GetMask("Ground")) && canFlip)
+            Flip();
+    }
+    void Flip()
+    {
+        patrolMoveSpeed = -patrolMoveSpeed;
+        transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+        canFlip = false;
+    }
+
+    void ChasePlayer()
+    {
+        float chaseOrientation = Mathf.Sign(playerTransform.position.x - faceCollider.transform.position.x);
+        patrolMoveSpeed = Math.Abs(patrolMoveSpeed) * chaseOrientation;
+
+        // flip once then put flip on cooldown to prevent very fast spin when right underneath player
+        if (directionTowardsPlayer == 0 || canFlip)
+        {
+            directionTowardsPlayer = chaseOrientation;
+            canFlip = false;
+        }
+        MoveHorizontally(directionTowardsPlayer * aggroMoveSpeed, true);
+        transform.localScale = new(directionTowardsPlayer, 1, 1);
+        enemyJump.JumpToPlayer();
+    }
+
+    void MoveToSpawn()
+    {
+        float directionTowardsSpawn = Mathf.Sign(spawnPos.x - transform.position.x);
+        MoveHorizontally(directionTowardsSpawn * returnToSpawnSpeed, false);
+        transform.localScale = new(directionTowardsSpawn, 1, 1);
+
+        if (distToSpawnPos <= 1f)
+        {
+            isReturningToSpawn = false;
+            patrolMoveSpeed = Math.Abs(patrolMoveSpeed) * directionTowardsSpawn;
+        }
+    }
+
+    void Patrol()
+    {
+        if (distToSpawnPos >= aggroRange - distanceTolerance && canFlip)
+            Flip();
+        MoveHorizontally(patrolMoveSpeed);
+    }
+
+    void MoveHorizontally(float speed, bool isChasing = false)
+    {
+        if (feetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            myRigidbody.velocity = new Vector2(speed, 0);
+            // check if is chasing to prevent double jump (since it's already jumping if so)
+            if (!isChasing)
+                enemyJump.JumpOnImminentCollision(Mathf.Sign(speed));
+        }
+    }
+}
