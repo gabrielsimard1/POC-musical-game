1b5db09d75ff87b64f101a33a6227b4e88025f47
gabrielsimard1 <gabrielsimard.1@hotmail.com>
2023-12-06 02:26:46 -0500

    wip

--- musical-game/Assets/Scripts/PlayerMovement.cs
+++ musical-game/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Game Object")]
     [SerializeField] BoxCollider2D feetCollider;
+
+    [Header("Movement Speed")]
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float dashSpeed = 15f;
     [SerializeField] float jumpSpeed = 5f;
+
+    [Header("Knockback")]
     [SerializeField] int knockbackForce = 20;
     [SerializeField] float knockbackTime = .5f;
 
@@ -19,18 +25,28 @@ public class PlayerMovement : MonoBehaviour
     Animator animator;
     bool canMove = true;
     float yAxisKnockBack = .5f;
+    float dashDuration = .2f;
+    bool isDashing;
+    bool dashInCooldown;
+    float initialGravityScale;
+
 
     void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        initialGravityScale = myRigidbody.gravityScale;
     }
 
     void FixedUpdate()
     {
-        if (canMove) // constant movement was interfering with the knockback. Need to not take input into account to be able to knockback
+        if (canMove && !isDashing) // constant movement was interfering with the knockback. Need to not take input into account to be able to knockback
         {
             Move();
+        }
+        if(dashInCooldown && isGrounded)
+        {
+            dashInCooldown = false; // reset dash cooldown
         }
 
     }
@@ -58,6 +74,15 @@ public class PlayerMovement : MonoBehaviour
             myRigidbody.velocity += new Vector2(0f, jumpSpeed);
         }
     }
+    void OnDash(InputValue value)
+    {
+        if (!canMove || dashInCooldown)
+            return;
+        if (value.isPressed)
+        {
+            StartCoroutine(Dash());
+        }
+    }
 
     void FlipSprite(bool hasHorizontalSpeed)
     {
@@ -77,6 +102,37 @@ public class PlayerMovement : MonoBehaviour
         animator.enabled = !died;
         canMove = !died;
     }
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        animator.SetBool("isWalking", false);
+        animator.SetBool("isDashing", true);
+        ToggleGravity(true);
+        Vector2 dashDirection = new(Mathf.Sign(transform.localScale.x) * dashSpeed, 0f);
+
+        if (isGrounded)
+            myRigidbody.AddForce(dashDirection, ForceMode2D.Impulse);
+        else
+            myRigidbody.velocity = dashDirection; // if mid-air, lock y axis in place (otherwise if quick jump + dash, player dash upwards higher than jump height)
+
+        yield return new WaitForSeconds(dashDuration);
+
+        ToggleGravity(false);
+        animator.SetBool("isDashing", false);
+        isDashing = false;
+        dashInCooldown = true;
+    }
+
+    void ToggleGravity(bool disableGravity)
+    {
+        // disable gravity so mid-air dash can stay mid-air
+        // don't disable gravity if grounded, otherwise dash + jump makes it jump absurdly high
+        if (disableGravity && !isGrounded)
+            myRigidbody.gravityScale = 0;
+        else
+            myRigidbody.gravityScale = initialGravityScale;
+    }
+
 
     bool DoesPlayerHaveHorizontalSpeed()
     {
